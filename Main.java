1b1c2e599b604a00c17b5f12f652f513bf71db6e
WellingtonGrein <144291768+WellingtonGrein@users.noreply.github.com>
2024-09-06 23:19:23 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // EQUACAO SEGUNDO GRAU
+       
         EquacaoSegundoGrau equacao = new EquacaoSegundoGrau(1,6,1);
         List<Double> resultados = equacao.calculaEquacaoSegundoGrau();
         System.out.println("RESULTADO EQUACAO=" + resultados);
 
-        // DESVIO PADRAO
         List<Double> lista = List.of(1.80,1.70,1.55);
         DesvioPadrao desvio = new DesvioPadrao(lista);
         System.out.println("RSULTADO DESVIO=" + desvio.calculaDesvioPadrao());
 
-        // JUROS COMPOSTO
         Juros juros = new Juros(7000,12,1);
         System.out.println("RESULTADO JUROS=" + juros.jurosComposto());
 
